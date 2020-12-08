b40cfc7da26274650638da789c1ac9ea9b947f9e
crgooch <crgooch@crimson.ua.edu>
2020-12-07 21:07:20 -0600

    first check in

---
+++ PA6/PA6/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PA6
+{
+    public partial class frmCWID : Form
+    {
+        public frmCWID()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClsoe_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
